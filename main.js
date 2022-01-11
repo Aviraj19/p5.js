8e6f2f6f40abbe6fad5b8bdbbad02dfe56f8ded0
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-01-11 15:52:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+nosex=""
+nosey=""
 function preload() {
 
 }
 
 function setup() {
-    canvas = createCanvas(600, 600);
+    canvas = createCanvas(600, 500);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(600,500);
+    video.hide();
+    poseNet= ml5.poseNet(video,modeloaded)
+    poseNet.on('pose',getposes)
 }
-
+function modeloaded() {
+    console.log("poseNet model is loaded")
+}
+function getposes(results) {
+    if(results.length>0) {
+  console.log(results);
+  nosex= results[0].pose.nose.x;
+  nosey= results[0].pose.nose.y;
+  console.log(nosex);
+  console.log(nosey)
+    }
+}
 function draw() {
-
+image(video,0,0,600,500)
 }
 
 function takesnapshot() {
